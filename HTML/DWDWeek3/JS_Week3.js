8d98495656db8ffbf9b0adbc628e027a6e82f19d
ItsWenHo <hh3066@nyu.edu>
2023-04-12 12:20:05 -0400

    Add files via upload

---
+++ HTML/DWDWeek3/JS_Week3.js
@@ -0,0 +1,40 @@
+
+const input = document.querySelector("#input");
+const city = document.querySelector("#city");
+
+const cityName = document.querySelector("#cityName");
+const Temp = document.querySelector("#temp");
+const main = document.querySelector("#main");
+const discription = document.querySelector("#discription");
+const image = document.querySelector("#image");
+
+input.onsubmit = (e) => {
+  e.preventDefault();
+  weatherUpdate(city.value);
+  city.value = "";
+};
+
+weatherUpdate = (city) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open(
+    "GET",
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3b76ba18775c44c0126f64755641905e`);
+
+  xhr.send();
+  xhr.onload = () => {
+    if (xhr.status === 404) {
+      alert("Place not found");
+    } else {
+      var data = JSON.parse(xhr.response);
+      cityName.innerHTML = data.name;
+      Temp.innerHTML = `${Math.round(data.main.temp - 273.15)}°C`;
+      main.innerHTML = data.weather[0].main;
+      discription.innerHTML = data.weather[0].description;
+     
+    }
+  };
+};
+
+weatherUpdate("brooklyn");
+
+
